00b161d280e17b6fbb9b14901bbfdd6ca028eac8
DrewWuzHere <drewsmith907@icloud.com>
2025-02-04 11:27:55 -0700

    added crit and hit to weapons

--- src/Backend/Weapons/Weapon.java
+++ src/Backend/Weapons/Weapon.java
@@ -5,12 +5,16 @@ public abstract class Weapon {
     String name;
     double dmg;
     String effect;
+    double crit;
+    double hit;
 // double durability???
 
-    public Weapon(String name, double dmg, String effect) {
+    public Weapon(String name, double dmg, String effect, double crit, double hit) {
         this.name = name;
         this.dmg = dmg;
         this.effect = effect;
+        this.crit = crit;
+        this.hit = hit;
     }
 
     public abstract void attack();
--- src/Backend/Weapons/WeaponsTest.java
+++ src/Backend/Weapons/WeaponsTest.java
@@ -2,13 +2,13 @@ package Backend.Weapons;
 
 public class WeaponsTest {
     public static void main(String[] args) {
-        Sword FlameSword = new Sword("Dog poop", 200000, "fire");
+        Sword FlameSword = new Sword("Dog poop", 200000, "fire", 0.035, 0.80);
         FlameSword.attack();
 
-        Bow LightBow = new Bow("Bow", 200000, "light");
+        Bow LightBow = new Bow("Bow", 200000, "light", 0.2, 0.65);
         LightBow.attack();
 
-        Axe NormalAxe = new Axe("Bow", 1, "none");
+        Axe NormalAxe = new Axe("Bow", 1, "none", 0.15, 0.70);
         NormalAxe.attack();
     }
 }
