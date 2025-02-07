fd1725e20c1839674ec71ee9f0689e263467f0da
Christian Figueroa <christianfigueroa@Christians-MacBook-Pro-2.local>
2025-02-07 16:14:25 -0700

    COllision fixed

--- src/Display/CollisionChecker.java
+++ src/Display/CollisionChecker.java
@@ -12,9 +12,9 @@ public class CollisionChecker {
     public void checkTile(CharacterManager entity) {
 
         int entityLeftWorldX = entity.worldX + entity.solidArea.x;
-        int entityRightWorldX = entity.worldX + entity.solidArea.x;
+        int entityRightWorldX = entity.worldX + entity.solidArea.x + entity.solidArea.width ;
         int entityTopWorldY = entity.worldY + entity.solidArea.y;
-        int entityBottomWorldY = entity.worldY + entity.solidArea.y;
+        int entityBottomWorldY = entity.worldY + entity.solidArea.y + entity.solidArea.height ;
 
         int entityLeftCol = entityLeftWorldX / gp.tileSize;
         int entityRightCol = entityRightWorldX / gp.tileSize;
