328659fcffe52d9cecba12022ce1f1d279ba6436
Christian Figueroa <christianfigueroa@Christians-MacBook-Pro-2.local>
2025-02-11 11:06:34 -0700

    Hi cutie

--- src/Backend/Weapons/WeaponFactory.java
+++ src/Backend/Weapons/WeaponFactory.java
@@ -3,13 +3,26 @@ package Backend.Weapons;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-public class WeaponsTest {
+public class WeaponFactory {
     public static void main(String[] args) {
 
 
         System.out.println("Current working directory: " + System.getProperty("user.dir"));
         Weapon WindsweptAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
         BufferedImage AxeSprite = WindsweptAxe.getSprite();
+
+
+    }
+
+
+
+    public void createWeapons(){
+        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
+        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
+        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
+        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
+        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
+
 
 
     }
