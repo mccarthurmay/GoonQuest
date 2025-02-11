c06ff2e9950399d698ff25d2ea490d7931fa67ee
Christian Figueroa <christianfigueroa@Christians-MacBook-Pro-2.local>
2025-02-11 12:00:30 -0700

    jdjdjd

--- src/Backend/Weapons/WeaponFactory.java
+++ src/Backend/Weapons/WeaponFactory.java
@@ -17,11 +17,16 @@ public class WeaponFactory {
 
 
     public void createWeapons(){
-        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
-        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
-        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
-        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
-        Weapon basicAxe = new Weapon("Windswept Axe", 10, "wind", 5,100,"Man, it's windy", "./src/Backend/Images/weaponSprites/default.png");
+        Weapon battleAxe = new Weapon("Battle Axe", 25, "wind", 8,85,"Man, it's windy", "./src/Backend/Images/Weapons/Axe/Sprite.png");
+        Weapon normalAxe = new Weapon("Basic Axe", 8, "wind", 5,85,"Man, it's windy", "./src/Backend/Images/Weapons/AxeTool/Sprite.png");
+        Weapon bigSword = new Weapon("Big sword", 28, "wind", 7,85,"Man, it's windy", "./src/Backend/Images/Weapons/BigSword/Sprite.png");
+        Weapon club = new Weapon("Club", 18, "wind", 15,99,"Man, it's windy", "./src/Backend/Images/Weapons/Club/Sprite.png");
+        Weapon Lance = new Weapon("Knight's lance", 22, "wind", 11,90,"Man, it's windy", "./src/Backend/Images/Weapons/Lance/Sprite.png");
+        Weapon katana = new Weapon("Kusanagi-no-tsurugi", 30, "wind", 10,80,"Man, it's windy", "./src/Backend/Images/Weapons/Katana/Sprite.png");
+        Weapon hammer = new Weapon("Sledgehammer.", 14, "wind", 12,99,"Man, it's windy", "./src/Backend/Images/Weapons/Hammer/Sprite.png");
+        Weapon sword = new Weapon("Dawnbreaker", 20, "wind", 20,100,"Man, it's windy", "./src/Backend/Images/Weapons/Sword/Sprite.png");
+
+
 
 
 
