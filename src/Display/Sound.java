5c5d11d66d547335f018bd32ee156d38178de510
Christian Figueroa <christianfigueroa@Christians-MacBook-Pro-2.local>
2025-02-11 00:45:23 -0700

    Sound begginging

---
+++ src/Display/Sound.java
@@ -0,0 +1,35 @@
+package Display;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.net.URL;
+
+public class Sound {
+    Clip clip;
+    URL soundURL[] = new URL[30];
+
+    public Sound() {}
+
+    public void setFile(int i) {
+        try{
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundURL[i]);
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+
+        } catch (Exception e){
+        }
+    }
+    public void play() {
+        clip.start();
+    }
+
+    public void loop(){
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop(){
+        clip.stop();
+    }
+
+}
