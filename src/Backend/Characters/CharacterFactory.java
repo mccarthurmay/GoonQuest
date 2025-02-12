3d81c4cc5dbe5ad66f016bccee5a007bb1c75a10
Maximillian May <83667715+mccarthurmay@users.noreply.github.com>
2025-02-12 07:07:59 -0500

    rebalance

--- src/Backend/Characters/CharacterFactory.java
+++ src/Backend/Characters/CharacterFactory.java
@@ -30,20 +30,20 @@ public class CharacterFactory {
 
         // Define weapon data in a 2D array: {name, damage, element, crit, accuracy, description, sprite name}
         Object[][] weaponData = {
-                {"Battle Axe", 25, "wind", 8, 85, "Man, it's windy", "Axe"},
-                {"Basic Axe", 8, "wind", 5, 85, "Man, it's windy", "AxeTool"},
-                {"Big sword", 28, "wind", 7, 85, "Man, it's windy", "BigSword"},
-                {"Club", 18, "wind", 15, 99, "Man, it's windy", "Club"},
-                {"Knight's lance", 22, "wind", 11, 90, "Man, it's windy", "Lance"},
-                {"Kusanagi-no-tsurugi", 30, "wind", 10, 80, "Man, it's windy", "Katana"},
-                {"Sledgehammer", 14, "wind", 12, 99, "Man, it's windy", "Hammer"},
-                {"Dawnbreaker", 20, "wind", 20, 100, "Man, it's windy", "Sword"},
+                {"Battle Axe", 55, "wind", 8, 85, "Man, it's windy", "Axe"},
+                {"Basic Axe", 20, "wind", 5, 85, "Man, it's windy", "AxeTool"},
+                {"Big sword", 37, "wind", 7, 85, "Man, it's windy", "BigSword"},
+                {"Club", 15, "wind", 15, 99, "Man, it's windy", "Club"},
+                {"Knight's lance", 35, "wind", 11, 90, "Man, it's windy", "Lance"},
+                {"Kusanagi-no-tsurugi", 40, "wind", 10, 80, "Man, it's windy", "Katana"},
+                {"Sledgehammer", 38, "wind", 12, 99, "Man, it's windy", "Hammer"},
+                {"Dawnbreaker", 23, "wind", 20, 100, "Man, it's windy", "Sword"},
                 {"Poseidon's Fork", 35, "wind", 30, 93, "Man, it's windy", "Fork"},
-                {"The 'Chucks", 15, "wind", 3, 40, "Man, it's windy", "Ninjaku"},
+                {"The 'Chucks", 20, "wind", 3, 40, "Man, it's windy", "Ninjaku"},
                 {"Fortnite", 1, "wind", 0, 0, "Man, it's windy", "Pickaxe"},
-                {"Pointy", 12, "wind", 10, 85, "Man, it's windy", "Rapier"},
-                {"Trunk", 15, "wind", 50, 90, "Man, it's windy", "Sai"},
-                {"Mid Sword", 10, "wind", 20, 85, "Man, it's windy", "Sword2"},
+                {"Pointy", 40, "wind", 10, 85, "Man, it's windy", "Rapier"},
+                {"Trunk", 32, "wind", 50, 90, "Man, it's windy", "Sai"},
+                {"Mid Sword", 15, "wind", 20, 85, "Man, it's windy", "Sword2"},
                 {"Lucky Whip", 99999999, "", 100, 1, "", "Whip"}
         };
 
@@ -120,15 +120,13 @@ public class CharacterFactory {
         items.add(new HpItem("Fish", 50, "./src/Backend/Images/itemSprites/Fish.png"));
 
 
-
-
         ArrayList<Item> blankItems = new ArrayList<>();
-        Stats gHoly = new Stats(15.0, 8.0, 1, 0.1, 0.75); // angelic, high HP
-        Stats gRed = new Stats(20.0, 3.0, 300, 0.5, 0.3); // he sees red, low hit chance but it'll hurt when it connects!
-        Stats gVoid = new Stats(100.0, 200.0, 10.0, 0.0, 1); // super easy to kill, but oh boy will he CORRUPT you
-        Stats gKawaii = new Stats(5.0, 20.0, 600.0, 0.5, 0.8); // kawaii!
-        Stats gStar = new Stats(10.0, 50.0, 300.0, 0.7, 0.99); // skilled sharpshooter. rarely misses.
-        Stats gSad = new Stats(15.0, 3.0, 200.0, 0.5, 0.3); // he's doing his best, alright?
+        Stats gHoly = new Stats(40.0, 8.0, 400, 0.1, 0.75); // angelic, high HP
+        Stats gRed = new Stats(20.0, 3.0, 60, 0.5, 0.5); // he sees red, low hit chance but it'll hurt when it connects!
+        Stats gVoid = new Stats(50.0, 200.0, 400.0, 0.0, 1); // super easy to kill, but oh boy will he CORRUPT you
+        Stats gKawaii = new Stats(10.0, 20.0, 600.0, 0.5, 0.8); // kawaii!
+        Stats gStar = new Stats(30.0, 50.0, 300.0, 0.7, 0.99); // skilled sharpshooter. rarely misses.
+        Stats gSad = new Stats(15.0, 3.0, 500.0, 0.5, 0.9); // he's doing his best, alright?
         Weapon lameStick = new Weapon ("Lame stick", 1.0, "None", 0.2, 0.3, "Ehhhh.. it's alright.", "");
 
         Enemy holyEnemy = new Enemy(gHoly, lameStick, "Glargo Holy", blankItems, gp, "src/Backend/Images/sprites/glargo_holy.png", 10864, 6824);
@@ -150,7 +148,7 @@ public class CharacterFactory {
          * Create default stats
          */
 
-        Stats stats = new Stats(100.0, 10.0, 200.0, 0.15, 0.85);
+        Stats stats = new Stats(20.0, 10.0, 200.0, 0.05, 0.85);
 
         /**
          * Create and return the hero
