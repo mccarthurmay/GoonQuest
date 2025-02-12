4748c12fca3b7c6aeb48059fefcec7412e6400a2
DrewWuzHere <drewsmith907@icloud.com>
2025-02-12 01:31:57 -0700

    risk of rain 2 is a really fun videogame and if you're reading this please try it

--- src/Backend/WorldBuilding/Tile.java
+++ src/Backend/WorldBuilding/Tile.java
@@ -2,6 +2,9 @@ package Backend.WorldBuilding;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Tile objects just store whether they have collision, and their image.
+ */
 public class Tile {
     public BufferedImage image;
     public boolean collision = false;
--- src/Display/Battle.java
+++ src/Display/Battle.java
@@ -9,16 +9,23 @@ public class Battle {
     JFrame window;
     BattlePanel battlePanel;
 
+    /**
+     * Constructor for battle class
+     * @param gp gamepanel to be drawn on
+     * @param window the window that will be drawn inside of (we will be making a new gamepanel for battles)
+     */
     public Battle(GamePanel gp, JFrame window) {
         this.gp = gp;
         this.window = window;
     }
 
-
-    // REMOVE THIS LATER
+    /**
+     * Function to handle the transition between the overworld gamepanel and the battle gamepanel
+     * @param enemy the enemy that will be fought in the battle (just one v one battles here)
+     */
     public void toBattleTransition(Enemy enemy) {
-        gp.stopGameThread();
-        gp.stopMusic();
+        gp.stopGameThread(); // stop the overworld logic
+        gp.stopMusic(); // stop the overworld music
 
         // Clean up old battle panel if it exists
         if (battlePanel != null) {
@@ -26,10 +33,10 @@ public class Battle {
             battlePanel = null;
         }
 
-        window.remove(gp);
-        gp.playMusic(2);
+        window.remove(gp); // remove the current game panel
+        gp.playMusic(2); // play the new music
 
-        battlePanel = new BattlePanel(gp, enemy, window);
+        battlePanel = new BattlePanel(gp, enemy, window); // create a new battle panel object with our enemy, window, and game panel
 
         // Switch to battle panel
         window.add(battlePanel);
@@ -38,11 +45,14 @@ public class Battle {
         window.repaint();
     }
 
+    /**
+     * update the battle
+     */
     public void update(){
-        if (battlePanel != null) {
-            battlePanel.update();
+        if (battlePanel != null) { // assuming the battle panel exists,
+            battlePanel.update(); // update the battle panel
         }
 
     }
 
-    }
+}
